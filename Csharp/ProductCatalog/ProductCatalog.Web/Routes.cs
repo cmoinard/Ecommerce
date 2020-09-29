9db2df70bbac7e553fcf6b924846324083d795b0
Christophe Moinard <cmoinard@lucca.fr>
2020-09-29 13:46:45 +0200

    Initialize Categories folder

--- Csharp/ProductCatalog/ProductCatalog.Web/Routes.cs
+++ Csharp/ProductCatalog/ProductCatalog.Web/Routes.cs
@@ -3,6 +3,5 @@ namespace ProductCatalog.Web
     public static class Routes
     {
         public const string Categories = "api/categories";
-        public const string Products = "api/products";
     }
 }
