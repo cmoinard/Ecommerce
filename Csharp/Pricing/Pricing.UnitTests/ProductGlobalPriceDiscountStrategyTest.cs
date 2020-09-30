ecc9eac5f9935e1feeb7599df59b16b75a65a8d1
Christophe Moinard <cmoinard@lucca.fr>
2020-09-30 10:57:49 +0200

    Unit tests for global strategies

--- Csharp/Pricing/Pricing.UnitTests/ProductGlobalPriceDiscountStrategyTest.cs
+++ Csharp/Pricing/Pricing.UnitTests/ProductGlobalPriceDiscountStrategyTest.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using NFluent;
 using Pricing.Hexagon.Discounts;
 using Pricing.Hexagon.ProductDiscounts.Aggregate.Strategies;
+using Pricing.Hexagon.Products.Aggregate;
 using Shared.Domain;
 using Xunit;
 
 namespace Pricing.UnitTests
 {
-    public class ProductGlobalWeightDiscountStrategyTest
+    public class ProductGlobalPriceDiscountStrategyTest
     {
         private static ProductId ProductId1 { get; } = new ProductId(Guid.NewGuid());
         private static ProductId ProductId2 { get; } = new ProductId(Guid.NewGuid());
@@ -18,23 +19,23 @@ namespace Pricing.UnitTests
         private static ProductId ProductId6 { get; } = new ProductId(Guid.NewGuid());
         private static ProductId ProductId7 { get; } = new ProductId(Guid.NewGuid());
         
-        private readonly IReadOnlyDictionary<ProductId, Weight> _weightByProductId =
-            new Dictionary<ProductId, Weight>
+        private readonly IReadOnlyDictionary<ProductId, Price> _weightByProductId =
+            new Dictionary<ProductId, Price>
             {
-                [ProductId1] = Weight.Grams(10),
-                [ProductId2] = Weight.Grams(500),
-                [ProductId3] = Weight.Kg(10),
-                [ProductId4] = Weight.Kg(20),
-                [ProductId5] = Weight.Kg(49.99m),
-                [ProductId6] = Weight.Kg(50),
-                [ProductId7] = Weight.Kg(80)
+                [ProductId1] = new Price(0),
+                [ProductId2] = new Price(10),
+                [ProductId3] = new Price(100),
+                [ProductId4] = new Price(200),
+                [ProductId5] = new Price(499.99m),
+                [ProductId6] = new Price(500),
+                [ProductId7] = new Price(800)
             };
 
         private readonly IProductGlobalDiscountStrategy _strategy;
 
-        public ProductGlobalWeightDiscountStrategyTest()
+        public ProductGlobalPriceDiscountStrategyTest()
         {
-            _strategy = new ProductGlobalWeightDiscountStrategy(_weightByProductId);
+            _strategy = new ProductGlobalPriceDiscountStrategy(_weightByProductId);
         }
         
         public static TheoryData<IReadOnlyCollection<ProductId>> NoDiscountProductIds =
@@ -48,7 +49,7 @@ namespace Pricing.UnitTests
         
         [Theory]
         [MemberData(nameof(NoDiscountProductIds))]
-        public void ShouldHaveNoDiscount_WhenWeightIsBelowThreshold(IReadOnlyCollection<ProductId> productIds)
+        public void ShouldHaveNoDiscount_WhenPriceIsBelowThreshold(IReadOnlyCollection<ProductId> productIds)
         {
             Check.That(_strategy.GetDiscount(productIds))
                 .Equals(Discount.None);
@@ -64,10 +65,10 @@ namespace Pricing.UnitTests
         
         [Theory]
         [MemberData(nameof(WithDiscountProductIds))]
-        public void ShouldHave5PercentDiscount_WhenWeightIsHigherThanThreshold(IReadOnlyCollection<ProductId> productIds)
+        public void ShouldHave25EurosDiscount_WhenPriceIsHigherThanThreshold(IReadOnlyCollection<ProductId> productIds)
         {
             Check.That(_strategy.GetDiscount(productIds))
-                .Equals(Discount.Percent(5));
+                .Equals(Discount.Price(25));
         }
     }
 }
